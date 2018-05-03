e2dd0155dbd0dfef7a10e9b2a82f46615737a96f
qiugh <graphy.qiu@gmail.com>
2018-05-03 17:55:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,55 @@
+  class Flow {
+  constructor(ignoreError) {
+    super();
+    this.processors = [];
+    this.ignoreError = ignoreError;
+  }
+
+  add(processor, idx) {
+    if (!(processor instanceof Processor))
+      throw new Error('Not a Processor instance');
+    if (idx !== undefined) {
+      this.processors.splice(idx, 0, processor);
+    } else {
+      this.processors.push(processor);
+    }
+  }
+
+  remove(idx) {
+    if (idx !== undefined) {
+      return this.processors.splice(idx, 1);
+    }
+    return this.processors.pop();
+  }
+
+  execute(xargs, callback) {
+    let self = this;
+    let funcs = self.processors.map(processor => processor.func);
+    asyncFlow({ funcs: funcs, xargs: xargs, ignoreError: self.ignoreError }, callback);
+  }
+}
+
+class Processor {
+  super();
+  constructor(name, func, async) {
+    if (typeof name !== 'string' || name === '')
+      throw new Error('processor name must be valid string');
+    if (typeof func !== 'function')
+      throw new Error('processor func must be a function');
+    this.name = name;
+    this.func = func;
+    this.async = async;
+    if (!this.async) {
+      this.func = function (params, callback) {
+        callback(null, func(params));
+      }
+    }
+  }
+}
+
 function asyncFlow(options, callback) {
   if (!options) return;
-  let funcs = options.funcs, xargs = options.xargs, force = options.force;
+  let funcs = options.funcs, xargs = options.xargs, ignoreError = options.ignoreError;
   if (typeof funcs === 'function') funcs = [funcs];
   if (!(funcs instanceof Array) || !funcs.length) return;
   if (!funcs.every(func => typeof func === 'function')) return;
@@ -23,7 +72,7 @@ function asyncFlow(options, callback) {
     let func = funcs[funcIdx], xarg = xargs[xargIdx];
     func(xarg, function (error, result) {
       tmpResults.push({ error, result });
-      if (error && !force) {
+      if (error && !ignoreError) {
         if (typeof callback === 'function') {
           let e = new Error();
           e.step = funcIdx;
@@ -42,56 +91,4 @@ function asyncFlow(options, callback) {
   }
 }
 
-function processFlow(options, callback) {
-  if (!options) return;
-  let xarg = options.xarg, force = options.force, processors = options.processors;
-  if (!(processors instanceof Array) || !processors.length) return;
-  if (!(processors.every(processor => processor instanceof Processor))) return;
-
-  let processorIdx = 0, tmpResults = [];
-  executeProcessors();
-  function executeProcessors() {
-    if (processorIdx >= processors.length) {
-      if (typeof callback === 'function') callback(null, tmpResults);
-      return;
-    }
-    let processor = processors[processorIdx];
-    processor(xarg, function (error, result) {
-      tmpResults.push({ error, result });
-      if (error && !force) {
-        if (typeof callback === 'function') {
-          let e = new Error();
-          e.step = processorIdx;
-          callback(e, tmpResults);
-        }
-        return;
-      }
-      if (processors.length === 1) {
-        if (typeof callback === 'function') callback(null, tmpResults);
-        return;
-      }
-      processorIdx++;
-      executeProcessors();
-    });
-  }
-}
-
-class Processor {
-  super();
-  constructor(name, action, async) {
-    if (typeof name !== 'string' || name === '')
-      throw new Error('processor name must be valid string');
-    if (typeof action !== 'function')
-      throw new Error('processor action must be a function');
-    this.name = name;
-    this.async = async;
-    this.action = action;
-    if (!this.async) {
-      this.action = function (params, callback) {
-        callback(null, action(params));
-      }
-    }
-  }
-}
-
-module.exports = { asyncFlow, Processor, processFlow };
+module.exports = { asyncFlow, Processor, Flow };
